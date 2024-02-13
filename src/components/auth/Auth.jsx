58244e6f800c6c3188db324d7ed5b41819bfd216
Adilet Gazybekov <gazybekov11@gmail.com>
2024-02-13 12:32:00 +0600

    save

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,7 +1,58 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
+import { useAuth } from "../context/AuthContextProvider";
 
 const Auth = () => {
-  return <div></div>;
+  const {
+    user,
+    email,
+    password,
+    emailError,
+    passwordError,
+    setEmail,
+    setPassword,
+    setUser,
+    setEmailError,
+    setPasswordError,
+    handleRegister,
+    handleLogOut,
+    handleLogin,
+  } = useAuth();
+  console.log(emailError, passwordError);
+  return (
+    <div>
+      {/* <TextField
+        label="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        helperText={emailError}
+      />
+      <TextField
+        label="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        helperText={passwordError}
+      />
+      <Button onClick={handleRegister} variant="contained">
+        Register
+      </Button> */}
+      <TextField
+        label="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        helperText={emailError}
+      />
+      <TextField
+        label="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        helperText={passwordError}
+      />
+      <Button onClick={handleLogin} variant="contained">
+        Login
+      </Button>
+    </div>
+  );
 };
 
 export default Auth;
